612676499405b0f80ced19f6e90cb897cc45e630
Mrdheerajcs <mrdheerajtech@gmail.com>
2026-04-06 14:26:23 +0530

    ..

--- src/auth/apiClient.js
+++ src/auth/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "http://192.168.1.6:8080",
   timeout: 10000,
 });
 
